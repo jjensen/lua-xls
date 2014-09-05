bb650a06ca502fc244bc6c1996d299e3d279a4fb
Fan Zhou <zhoufan@nibirutech.com>
2014-09-05 14:06:05 +0800

    link lua lib

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-cc -fPIC -shared -o xls.so ExcelFormat/*.cpp *.cpp -IExcelFormat -I/usr/include/lua5.1 -lstdc++
+cc -fPIC -shared -o xls.so ExcelFormat/*.cpp *.cpp -IExcelFormat -I/usr/include/lua5.1 -lstdc++ -llua
