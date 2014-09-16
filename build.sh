9c6b9a2a288eddbd1f547e09e0cd7521ae659fbd
Fan Zhou <zhoufan@nibirutech.com>
2014-09-16 16:13:04 +0800

    build.sh LUA_ENV

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-cc -fPIC -shared -o xls.so ExcelFormat/*.cpp *.cpp -IExcelFormat -I/usr/include/lua5.1 -lstdc++ -llua
+cc -fPIC -shared -o xls.so ExcelFormat/*.cpp *.cpp -IExcelFormat -I$LUA_INCLUDE_PATH -lstdc++ -l$LUA_LIB
