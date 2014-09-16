3df3c9c615bbd5ca8bc3f56f846969cb3eb1d2b3
Fan Zhou <zhoufan@nibirutech.com>
2014-09-16 17:25:19 +0800

    build.sh depend on LUA_ENV

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-cc -fPIC -shared -o xls.so ExcelFormat/*.cpp *.cpp -IExcelFormat -I$LUA_INCLUDE_PATH -lstdc++ -l$LUA_LIB
+cc -fPIC -shared -o xls.so ExcelFormat/*.cpp *.cpp -IExcelFormat -I$LUA_INCLUDE_PATH -L$LUA_LIB_PATH -lstdc++ -l$LUA_LIB
